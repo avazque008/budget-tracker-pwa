4e3eb249cae3dc385ef45d1765e009c45e14a55c
Alex Vazquez <avazque008@outlook.com>
2022-07-10 16:30:41 -0400

    manifest file created

--- public/js/idb.js
+++ public/js/idb.js
@@ -2,16 +2,12 @@ let db;
 const request = indexedDB.open('budget_tracker', 1);
 
 request.onupgradeneeded = function (event) {
-
     const db = event.target.result;
-
     db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
 request.onsuccess = function (event) {
-
     db = event.target.result;
-
     if (navigator.onLine) {
 
         uploadTransaction();
@@ -19,31 +15,24 @@ request.onsuccess = function (event) {
 };
 
 request.onerror = function (event) {
-
     console.log(event.target.errorCode);
 };
 
 
-function saveRecord(data) {
-
+function saveRecord(record) {
     const transaction = db.transaction(['new_transaction'], 'readwrite');
-
-    const transactionObjectStore = transaction.objectStore('new_transaction');
-
-    transactionObjectStore.add(data);
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+    budgetObjectStore.add(record);
 };
 
 function uploadTransaction() {
-
     const transaction = db.transaction(['new_transaction'], 'readwrite');
-
-    const transactionObjectStore = transaction.objectStore('new_transaction');
-
-    const getAll = transactionObjectStore.getAll();
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+    const getAll = budgetObjectStore.getAll();
 
     getAll.onsuccess = function () {
         if (getAll.result.length > 0) {
-            fetch("/api/transaction", {
+            fetch("/api/transaction/bulk", {
                 method: "POST",
                 body: JSON.stringify(getAll.result),
                 headers: {
@@ -56,14 +45,14 @@ function uploadTransaction() {
                 })
                 .then(data => {
                     if (data.errors) {
-                        errorEl.textContent = "Missing Information";
+                        return errorEl.textContent = "Missing Information";
                     }
                     else {
                         const transaction = db.transaction(['new_transaction'], 'readwrite');
 
-                        const transactionObjectStore = transaction.objectStore('new_transaction');
+                        const budgetObjectStore = transaction.objectStore('new_transaction');
 
-                        transactionObjectStore.clear();
+                        budgetObjectStore.clear();
 
                         nameEl.value = "";
                         amountEl.value = "";
